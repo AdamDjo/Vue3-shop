34a04c3c7989cd7e81fc3b1a5e841d33865109d0
AdamDJo <adambenmessaoud2@gmail.com>
2024-02-23 14:00:12 +0100

    add icons & new header

---
+++ src/components/Header/Navbar.vue
@@ -0,0 +1,149 @@
+<template>
+  <header :class="active ? 'nav nav-mobile nav--active' : 'nav nav--mobile'">
+    <!--start nav-->
+
+    <div
+      :class="active ? 'nav__desktop nav__desktop--active' : 'nav__desktop'"
+      ref="navBar"
+    >
+      <!--nav__desktop-->
+      <div class="nav__logo nav__logo-desktop">
+        <router-link to="/">
+          <v-icon
+            name="pr-user"
+            fill="#69707D"
+            scale="1.5"
+            class="avatar__Icon"
+            @click="changeModalAuth"
+          />
+        </router-link>
+      </div>
+
+      <nav class="nav__links">
+        <router-link to="/Collections" class="nav__link nav__link--noSpace"
+          >Collections</router-link
+        >
+        <router-link to="/Men" class="nav__link">Men</router-link>
+        <router-link to="/Women" class="nav__link">Women</router-link>
+        <router-link to="/" class="nav__link">About</router-link>
+        <router-link to="/" class="nav__link">Contact</router-link>
+      </nav>
+
+      <div class="nav__cart nav__cart--desktop">
+        <div class="nav__cartShopping nav__cartShopping--desktop">
+          <v-icon
+            name="pr-user"
+            fill="#69707D"
+            scale="1.5"
+            class="avatar__Icon"
+            @click="changeModalAuth"
+          />
+          <span
+            class="nav__cartNumber nav__cartNumber--desktop"
+            @click="changeMiniCard"
+          ></span>
+        </div>
+
+        <div class="avatar__icon">
+          <v-icon
+            name="pr-user"
+            fill="#69707D"
+            scale="1.5"
+            class="avatar__Icon"
+            @click="changeModalAuth"
+          />
+          <article class="auth" v-if="isModalAuth && isLogin">
+            <AccountAuth />
+          </article>
+        </div>
+      </div>
+
+      <div class="nav__miniCard nav__miniCard--desktop" v-if="showMiniCard">
+        <MiniCard />
+      </div>
+    </div>
+    <!--end nav__desktop-->
+
+    <!--nav__mobile-->
+    <div class="nav__mobile">
+      <div class="nav__container">
+        <div class="nav__mobileContainer">
+          <div
+            :class="showMenu ? 'hambu hambu--active' : 'hambu'"
+            @click="toggleMenu"
+          ></div>
+
+          <div class="nav__mobileLinksContainer" v-if="showMenu">
+            <div class="nav__mobileLinks">
+              <nav class="nav__linksMobile">
+                <router-link to="/Collections" class="nav__linkMobile"
+                  >Collections</router-link
+                >
+                <router-link to="/Men" class="nav__linkMobile">Men</router-link>
+                <router-link to="/Women" class="nav__linkMobile"
+                  >Women</router-link
+                >
+                <router-link to="/" class="nav__linkMobile">About</router-link>
+                <router-link to="/" class="nav__linkMobile"
+                  >Contact</router-link
+                >
+              </nav>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!--end nav__mobile-->
+
+      <div class="nav__logo nav__logo--mobile">
+        <router-link to="/">
+          <v-icon
+            name="pr-user"
+            fill="#69707D"
+            scale="1.5"
+            class="avatar__Icon"
+            @click="changeModalAuth"
+          />
+        </router-link>
+      </div>
+
+      <div class="nav__cart">
+        <div class="nav__cartShopping">
+          <v-icon
+            name="pr-user"
+            fill="#69707D"
+            scale="1.5"
+            class="avatar__Icon"
+            @click="changeModalAuth"
+          />
+          <span class="nav__cartNumber"></span>
+        </div>
+
+        <div class="nav__avatarImg">
+          <!-- <img src="@/assets/images/image-avatar.png" alt="avatar" class="nav__avatar" /> -->
+          <v-icon
+            name="pr-user"
+            fill="#69707D"
+            scale="1.5"
+            class="avatar__Icon"
+          />
+          <article class="auth"></article>
+        </div>
+      </div>
+
+      <div class="nav__miniCard" v-if="showMiniCard">mini</div>
+    </div>
+    <!-- end nav__mobile
+    <Transition name="fade">
+      <section class="auth">transition</section>
+    </Transition> -->
+  </header>
+  <!--end nav-->
+</template>
+<style lang="scss">
+@import "./scss/Navbar.scss";
+</style>
+<script>
+export default {
+  name: "NavBar",
+};
+</script>
